f7715780583d35c9e1d788863d3436c43f8198a6
tiago-silva <cajuishop@gmail.com>
2026-07-18 15:19:46 -0300

    feat: add ChamaNoRateioPage

---
+++ app/pages/chama-no-rateio/page.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from 'next';
+import React from 'react';
+export const metadata: Metadata = {
+  title: 'Chama no Rateio Web',
+  keywords:
+    'Chama no Rateio Web, rateio, divir conta, econômia, controle de gastos',
+  description:
+    'Chama no Rateio é uma aplicação web para ajudar pessoas a gerenciar gastos no momento de dividar as contas',
+  authors: {
+    name: 'J. Tiago Silva',
+  },
+};
+const ChamanoRateioPage = () => {
+  return <main className="min-h-screen w-full"></main>;
+};
+
+export default ChamanoRateioPage;
